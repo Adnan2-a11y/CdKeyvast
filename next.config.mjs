ed12086e7035a7e79ecf49aa2d9eee17640c7550
Adnan2-a11y <inspiremedaily47@gmail.com>
2026-03-29 16:31:38 +0600

    fix[npm build]

--- next.config.mjs
+++ next.config.mjs
@@ -24,16 +24,13 @@ const nextConfig = {
     ],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    return config;
   },
 };
 
